613cb36ddb8e41e2dda4cd839b41dd2916c09a88
lookoupai <42715712+lookoupai@users.noreply.github.com>
2020-07-15 18:20:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ ! -f /opt/bookstack/dbupdated ]
+if [ ! -f /www/bookstack/dbupdated ]
 then
-  /opt/bookstack/BookStack install 2>&1 | tee /opt/bookstack/dbupdated
+  /www/bookstack/BookStack install 2>&1 | tee /www/bookstack/dbupdated
 else
   echo "###db had updated,skip bookstack install,just start it!###"
 fi
-/opt/bookstack/BookStack
+/www/bookstack/BookStack
